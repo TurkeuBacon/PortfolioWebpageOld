8548941db27b870fec5b9f5c7516313a9a5be66e
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2024-08-25 16:56:37 -0700

    Now loading project cards from JSON and about section from txt.

--- src/main.js
+++ src/main.js
@@ -1,32 +1,28 @@
-const MAX_PARENT_SEARCH_ITERATIONS = 10;
-let BASE = [location.protocol, "//", location.host].join('');
-console.log("BASE: " + BASE);
-if(BASE == "https://turkeubacon.github.io") {
-    console.log("GITHUB");
-    BASE += "/PortfolioWebpage";
-} else {
-    console.log("LOCAL");
-}
-
-document.body.onclick = function(e) {
-    let clicked = e.target;
-    let lastTr = null;
-    for(let i = 0; i < MAX_PARENT_SEARCH_ITERATIONS; i++) {
-        if(clicked.className == 'project_table') {
-            if(lastTr == null) return;
-            handleProjectClick(lastTr.id.substr(11));
-            break;
-        } else if(clicked.tagName == "TR") {
-            lastTr = clicked;
-        }
-        clicked = clicked.parentNode;
-        if(clicked == null || clicked == undefined) {
-            return;
-        }
+$(document).ready(function() {
+    try {
+        $.ajax({
+            url : "../text_files/about.txt",
+            success : function (data) {
+                document.getElementById("about_text").innerHTML = data;
+            }
+        });
+    } catch(error) {
+        document.getElementById("about_text").innerHTML = error;
     }
-}
-
-function handleProjectClick(projectId) {
-    console.log(BASE);
-    window.location.href = BASE + "/project_pages/" + projectId + ".html";
-}
+    
+    fetch('../project_pages/projectCards.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let cards = json.cards;
+            for(let i = 0; i < cards.length; i++) {
+                const card = cards[i];
+                let project_card_element = new ProjectCard();
+                project_card_element.setAttribute("id", "project_id-" + card.id);
+                project_card_element.setAttribute("title", card.title);
+                project_card_element.setAttribute("desc", card.desc);
+                project_card_element.setAttribute("img_src", card.img_src);
+                document.getElementById("projects_table_body").appendChild(project_card_element);
+            }
+        }
+    );
+});
